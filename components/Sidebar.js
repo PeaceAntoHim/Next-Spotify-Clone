05e7d6b97741b46acbc334be3421b98e97608b15
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-06 23:57:54 +0700

    building the sidebar components

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,12 +6,25 @@ import {
     HeartIcon,
     PlusCircleIcon,
 } from '@heroicons/react/outline';
+import { useState, useEffect } from 'react';
+import useSpotify from '../hooks/useSpotify';
 import { signOut, useSession } from 'next-auth/react';
 
 function Sidebar() {
+    const spotifyApi = useSpotify();
     /* Made Middleware to scure web */
-    const { data: session, status }  = useSession();
+    const { data: session, status }     = useSession();
+    /* made statment playlist */
+    const [playlists, setPlaylists] = useState([]);
 
+    useEffect(() => {
+        if (spotifyApi.getAccessToken()) {
+            spotifyApi.getUserPlaylists().then((data) => {
+                setPlaylists(data.body.items);    
+            });
+        }
+    }, [session, spotifyApi]);
+    console.log(playlists);
 
     return(
         <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
