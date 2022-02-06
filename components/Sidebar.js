c4c3982cd620aecacd9bac26132a000769ff0407
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-06 23:01:12 +0700

    building the center components

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -12,8 +12,9 @@ function Sidebar() {
     /* Made Middleware to scure web */
     const { data: session, status }  = useSession();
 
+
     return(
-        <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+        <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
             <div className="space-y-4">
                 <button 
                     className="flex items-center space-x-2 hover:text-white" 
