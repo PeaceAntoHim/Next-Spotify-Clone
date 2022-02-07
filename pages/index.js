289e4f6e5d4dbfe82510632cfbd9e5af10b3d594
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-07 17:38:47 +0700

    fix bug default picture

--- pages/index.js
+++ pages/index.js
@@ -30,8 +30,8 @@ export async function getServerSideProps(context) {
   const session = await getSession(context);
 
   return {
-    props: (
-      session
-    ),
+    props: {
+      session,
+    },
   };
 }
